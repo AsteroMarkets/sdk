c76208ed2165719fd9e883c2d2af45fd2d7f0c55
AsteroMarkets <yunabest190@gmail.com>
2026-02-16 18:28:48 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import SwapBox from "../components/SwapBox";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <SwapBox />
+    </div>
+  );
+}

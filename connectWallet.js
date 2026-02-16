1eb2d3061e8575d2f27b48995cbd2fea1c82ea7d
AsteroMarkets <yunabest190@gmail.com>
2026-02-16 18:29:26 +0700

    Create connectWallet.js

---
+++ connectWallet.js
@@ -0,0 +1,8 @@
+export async function connectWallet() {
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    alert("Wallet Connected");
+  } else {
+    alert("Install MetaMask");
+  }
+}
